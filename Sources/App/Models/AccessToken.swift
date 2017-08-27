7d35b4bcbdd6172d5c159564e4d5e7fee38a3ca8
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2017-08-27 20:37:29 +0900

    Temporary implementation of authentication with token

---
+++ Sources/App/Models/AccessToken.swift
@@ -0,0 +1,61 @@
+import Vapor
+import FluentProvider
+
+final class AccessToken: Model {
+    let storage = Storage()
+    
+    let token: String
+    let userId: Identifier
+    
+    static let tokenKey = "token"
+    
+    init(token: String, userId: Identifier) {
+        self.token = token
+        self.userId = userId
+    }
+    
+    init(row: Row) throws {
+        token = try row.get(AccessToken.tokenKey)
+        userId = try row.get(User.foreignIdKey)
+    }
+    
+    func makeRow() throws -> Row {
+        var row = Row()
+        try row.set(AccessToken.tokenKey, token)
+        try row.set(User.foreignIdKey, userId)
+        return row
+    }
+}
+
+extension AccessToken: Preparation {
+    static func prepare(_ database: Database) throws {
+        try database.create(self) { builder in
+            builder.id()
+            builder.string(AccessToken.tokenKey)
+            builder.parent(User.self)
+        }
+    }
+    
+    static func revert(_ database: Database) throws {
+        try database.delete(self)
+    }
+}
+
+extension AccessToken: JSONConvertible {
+    convenience init(json: JSON) throws {
+        try self.init(
+            token: json.get(AccessToken.tokenKey),
+            userId: json.get(User.foreignIdKey)
+        )
+    }
+    
+    func makeJSON() throws -> JSON {
+        var json = JSON()
+        try json.set(AccessToken.tokenKey, token)
+        return json
+    }
+}
+
+extension AccessToken: ResponseRepresentable {}
+
+extension AccessToken: Timestampable {}
