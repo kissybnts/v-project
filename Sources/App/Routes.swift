48d946f0fc23b7c886ed1c1b0c35086b43f323c6
Masataka Kishida <fstt.fwmym.f15@gmail.com>
2017-08-27 21:11:33 +0900

    Removed Post classes

--- Sources/App/Routes.swift
+++ Sources/App/Routes.swift
@@ -4,26 +4,6 @@ import AuthProvider
 extension Droplet {
     func setupRoutes() throws {
         let userController = UserController()
-        get("hello") { req in
-            var json = JSON()
-            try json.set("hello", "world")
-            return json
-        }
-
-        get("plaintext") { req in
-            return "Hello, world!"
-        }
-
-        // response to requests to /info domain
-        // with a description of the request
-        get("info") { req in
-            return req.description
-        }
-
-        get("description") { req in return req.description }
-        
-        try resource("posts", PostController.self)
-        
         
         // able to access without token
         group("v1") { unAuthed in
